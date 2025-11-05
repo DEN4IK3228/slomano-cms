1bc7735ad56f4823588f0f545413b0435f94a9bd
DEN4IK3228 <slomano.prod@gmail.com>
2025-11-06 03:19:38 +0400

    Fix Netlify: publish=dist + SPA redirects

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  }
+  // build: { outDir: 'dist' } // можно явно указать, но по умолчанию и так dist
 })
