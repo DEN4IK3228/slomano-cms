a6b4afdfb81b199c3ae173ee0e30417a91986031
DEN4IK3228 <slomano.prod@gmail.com>
2025-11-07 20:52:27 +0400

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,4 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-    },
-  },
+// cms/config/plugins.ts
+export default () => ({
+  upload: { config: { provider: 'local' } }
 });
