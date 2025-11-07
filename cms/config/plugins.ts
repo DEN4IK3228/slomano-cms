d44c54a65fa9a2fc11f238222fa24a19dfe90da8
DEN4IK3228 <slomano.prod@gmail.com>
2025-11-07 21:05:55 +0400

    chore(upload): use Cloudinary provider

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,16 +1,17 @@
-export default ({ env }) => ({
+// cms/config/plugins.ts
+export default () => ({
   upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloudinary: {
-          // если используешь CLOUDINARY_URL одной строкой
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
       },
-      sizeLimit: 50 * 1024 * 1024,
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
