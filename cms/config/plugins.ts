07bab4b658a622f627fdc1a6d773ad42b8217203
DEN4IK3228 <slomano.prod@gmail.com>
2025-11-19 03:04:58 +0400

    Remove Cloudinary and switch to local uploads

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,17 +1,1 @@
-// cms/config/plugins.ts
-export default () => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET,
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
-});
+module.exports = () => ({});
